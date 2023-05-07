330573269c5e2417c848bf9bbcaac3f08d1c43d4
isanchitaB <131647100+isanchitaB@users.noreply.github.com>
2023-05-07 21:13:19 +0530

    Update plotting sediment core data on map.R

--- plotting sediment core data on map.R
+++ plotting sediment core data on map.R
@@ -1,21 +1,24 @@
 require(tidyverse)
 require(ggplot2)
-require(ggspatial)
+require(ggspatial)#'ggspatial' package can calculate the distance between the points with different coordinates and assignment of accurate scale.
+#These are the required packages for making a plot with multiple layers.
 world_coordinates <- map_data("world") #importing available data on world coordinates in R environment
 View(world_coordinates)
+
 ggplot() +
   geom_map(
     data = world_coordinates, map = world_coordinates,
     aes(long, lat, map_id = region),color = "brown", fill= "lightyellow")+
   geom_label(data = total, aes(total$X3, total$X5,label = total$X1),size =2.5, fill = 'white', hjust =1, nudge_x = 1)+ 
-  geom_point(data = total, aes(total$X3, total$X5, color = total$Ocean,label = rownames(total$X1)))+ #Adding external sampling location data on basemap
+  geom_point(data = total, aes(total$X3, total$X5, color = total$Ocean,label = rownames(total$X1)))+ #Adding external sampling location data on basemap. 'Total' is the 
+#datafile which contains the input arranged in the form of latitude and logitudes as y and x. Label can be added based on core number or manually. 
   scale_color_manual(values = c('red', 'darkgreen', 'darkblue', 'orange4', 'violetred'))+
   coord_cartesian(xlim = c(-180,180))+
   labs(x = "Longitude", y ="Latitude", color = "Ocean Region")+
   annotation_north_arrow(location = "tl", which_north = "true", 
                          pad_x = unit(0.2, "in"), pad_y = unit(0.2, "in"),
                          style = north_arrow_fancy_orienteering)+
-  annotate("text", label = "North \nAtlantic \nOcean",x = -43, y = 20)+ # Highlighting the major ocean basin
+  annotate("text", label = "North \nAtlantic \nOcean",x = -43, y = 20)+ # Highlighting the major ocean basin using annotation of text
   annotate("text", label = "South \nAtlantic \nOcean",x = -18, y = -12)+
   annotate("text", label = "Southern Ocean",x = -25, y = -60)+
   annotate("text", label = "North \nPacific \nOcean",x = -150, y = 30)+
@@ -27,6 +30,6 @@ ggplot() +
   theme(legend.position = c(0.15,0.13), legend.background = element_rect(color = FALSE, fill = FALSE),legend.box.background =element_rect(color = "Black", fill = "white")  ,  legend.direction = "vertical",legend.box = "horizontal",  legend.box.margin = margin(t=1, b=8, r=2, l=2, unit = "pt") )+
   theme(axis.title = element_text(size = 12, colour = "Black", face = 'bold' ))+ 
   theme(axis.text = element_text(size = 12, colour = "Black" ))+
-  theme(panel.background = element_rect(fill = "paleturquoise"))
+  theme(panel.background = element_rect(fill = "paleturquoise"))# Fill colours can be changed 
 
 
